438947f4854bcb154fa4c8d3300da5857c561053
mherkarapetyan98 <mherkarapetyan4@gmail.com>
2019-02-25 15:45:26 +0400

    project installed and started successfully

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome</h1>
+                <h1>Hello redux</h1>
             </div>
         )
     }
